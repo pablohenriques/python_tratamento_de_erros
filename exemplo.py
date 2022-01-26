cde34180b681a097391b070d4400884302b2eb71
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-01-25 22:53:02 -0300

    Tratamento de Excecoes[2/7]

--- exemplo.py
+++ exemplo.py
@@ -1,19 +1,33 @@
 def dividir(dividendo, divisor):
-    # print(divisor.resultado)
-    return dividendo / divisor
-
-def testa_divisao(divisor):
+    if not (isinstance(dividendo, int) and isinstance(divisor, int)):
+        raise ValueError("dividir() deve apenas receber argumentos inteiros")
     try:
-        resultado = dividir(10, divisor)
-        print(f"10 / {divisor} = {resultado}")
-    except ZeroDivisionError:
-        print("Erro de disivão por zero tratado")
-    except AttributeError:
-        print("Erro de atributo tratado")
+        aux = dividendo / divisor
+    except:
+        print(f"Não foi possível dividir {dividendo} por {divisor}")
+        raise
+    return aux
+
+def testa_divisao(divisor):    
+    resultado = dividir(10, divisor)
+    print(f"10 / {divisor} = {resultado}")
+
+
+# try:
+#     testa_divisao(2.5)
+# except Exception as e:
+#     print(e)
+# except ZeroDivisionError as e:
+#     # print("Erro de disivão por zero tratado")
+#     print("Erro divisão por zero")
+# except Exception as e:
+#     # print("Erro de atributo tratado")
+#     print("Tratamento genérico")
+# print(f"Programa Encerrado")
 
 try:
-    testa_divisao(0)
-except AttributeError:
-    print("Erro de atributo tratado")
-
-print(f"Programa Encerrado")
+    print("O fluxo está aqui <----")
+    raise ValueError
+except Exception:
+    print("Agora ele foi para cá ---->")
+print("E enfim, ele continua ...")
